6066a810ec45478fe89fcc823187431f8afc0ff0
maryia-deriv <maryia@deriv.com>
2021-11-15 16:17:52 +0300

    Maryia/styled QuickStartIntro

--- src/components/Documentation/QuickStart/QuickStart.tsx
+++ src/components/Documentation/QuickStart/QuickStart.tsx
@@ -1,22 +1,23 @@
 import React from "react";
+import { PATHS } from "utils";
+import styles from "./QuickStart.module.scss";
 
-type QuickStartPropsType = {
+type QuickStartIntroPropsType = {
     // props
 };
 
-const QuickStart: React.FC<QuickStartPropsType> = () => (
+const QuickStartIntro: React.FC<QuickStartIntroPropsType> = () => (
     <>
-        <h1 className="doc-main-title">Quickstart to Deriv API</h1>
-        <div>
+        <h1 className={styles["doc-main-title"]}>Quickstart to Deriv API</h1>
+        <div className={styles["quick-start-intro"]}>
             <p>
-                On this page, you&#39;ll find code samples in various programming languages showing you how to work with
-                the Deriv API to perform some of the most important operations.
+                On this page, you’ll find code samples in various programming languages showing you how to work with the
+                Deriv API to perform some of the most important operations.
             </p>
             <p>
-                You can find all of the other available calls in the{" "}
-                <a href="{{ branch }}/playground/">API Playground</a>.
+                You can find all of the other available calls in the <a href={PATHS.PLAYGROUND}>API Playground</a>.
             </p>
-            <h3 className="api-sub-title bold">Before you begin</h3>
+            <h3 className={`${styles["api-sub-title"]} bold`}>Before you begin</h3>
             <ul className="bullet">
                 <li>
                     Open a <a href="https://deriv.com/">Deriv account</a> (either a demo or real account).
@@ -28,7 +29,7 @@ const QuickStart: React.FC<QuickStartPropsType> = () => (
                     Register your app to receive your <b>app_id</b> or use <b>app_id 1089</b> to test Deriv API.
                 </li>
             </ul>
-            <h3 className="api-sub-title bold">Setting up your environment</h3>
+            <h3 className={`${styles["api-sub-title"]} bold`}>Setting up your environment</h3>
             <p>
                 Instructions for setting up your environment and running the examples in your desired programming
                 language are given as comments in the code samples.
@@ -37,4 +38,4 @@ const QuickStart: React.FC<QuickStartPropsType> = () => (
     </>
 );
 
-export default QuickStart;
+export default QuickStartIntro;
--- src/pages/docs/index.tsx
+++ src/pages/docs/index.tsx
@@ -1,16 +1,13 @@
 import DocsLayout from "components/Documentation/Layout/DocsLayout";
-import QuickStart from "components/QuickStart/QuickStart";
+import QuickStartIntro from "components/Documentation/QuickStart/QuickStart";
+import styles from "../../components/Documentation/QuickStart/QuickStart.module.scss";
 
 type PageComponentWithLayout = React.FC & {
     Layout: React.FC;
 };
 
 const Docs: PageComponentWithLayout = () => {
-    return (
-        <div>
-            <QuickStart />
-        </div>
-    );
+    return <QuickStartIntro />;
 };
 
 Docs.Layout = DocsLayout;
