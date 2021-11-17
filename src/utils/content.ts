8c2a5fea106104d398f369a0a473fe47e1c11f4c
maryia-deriv <maryia@deriv.com>
2021-11-17 18:03:44 +0300

    Maryia/returned data array back to QuickStartIntro

---
+++ src/utils/content.ts
@@ -0,0 +1,18 @@
+import { BenefitPropsType } from "../components/BenefitsSection/BenefitIcon/BenefitIcon";
+
+// Benefit section content
+
+export const benefit_icons_array: Array<BenefitPropsType> = [
+    {
+        title: "Automation",
+        imgPath: "/automation.svg",
+    },
+    {
+        title: "Easy integration",
+        imgPath: "/integration.svg",
+    },
+    {
+        title: "Fast execution",
+        imgPath: "/execution.svg",
+    },
+];
