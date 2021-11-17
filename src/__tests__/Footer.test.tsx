7eb5991df63eba10214eb0004e3be99e4dbb07d6
maryia-deriv <maryia@deriv.com>
2021-11-17 18:07:06 +0300

    Maryia/optimized imports in tests

--- src/__tests__/Footer.test.tsx
+++ src/__tests__/Footer.test.tsx
@@ -1,10 +1,10 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { render } from "@testing-library/react";
 import Footer from "components/common/Footer";
 
 describe("Footer", () => {
     it("Footer component is rendered", () => {
-        const result = render(<Footer/>);
+        const result = render(<Footer />);
         const fake_className = result.container.querySelector(".header-container");
 
         expect(result.container.querySelector("footer")).toBeInTheDocument();
@@ -12,7 +12,7 @@ describe("Footer", () => {
         expect(fake_className).not.toBeInTheDocument();
     });
     it("Footer contains the text: © 2021 Deriv | All rights reserved", () => {
-        const result = render(<Footer/>);
+        const result = render(<Footer />);
         const fake_text = result.queryByText(/Personalisation/i);
 
         expect(result.getByText("© 2021 Deriv | All rights reserved")).toBeInTheDocument();
